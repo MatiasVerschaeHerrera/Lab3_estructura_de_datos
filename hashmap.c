ca8b1c01ad504f55382fca07d88404fee3ea09b7
Your Name <you@example.com>
2026-04-16 20:18:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,8 +68,9 @@ void insertMap(HashMap * map, char * key, void * value) {
     while(map->buckets[pos] != 0){
         pos++;
     }
-    map->buckets[pos]->key = key;
-    map->buckets[pos]->value = value;
+    map->buckets[pos] = createPair(key, value)
+    //map->buckets[pos]->key = key;
+    //map->buckets[pos]->value = value;
     map->size++;
         //map->current = map->buckets[pos];
 }
