c7d61f622b8254c9d23bd6798222122d8daa0a91
Your Name <you@example.com>
2026-04-17 03:42:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -104,9 +104,11 @@ Pair * searchMap(HashMap * map,  char * key) {
 // No elimine el par, sólo invalídelo asignando NULL a la clave (pair->key=NULL). 
 // Recuerde actualizar la variable size.
 
-void eraseMap(HashMap * map,  char * key) {    
-
-
+void eraseMap(HashMap * map,  char * key) {
+    if(map == NULL) return NULL;
+    long pos = hash(key, map->capacity);
+    map->buckets[pos]->key == NULL;
+    map->size--;
 }
 
 // 5. Implemente las funciones para recorrer la estructura: Pair * firstMap(HashMap * map) retorna el primer Pair válido del arreglo buckets. 
